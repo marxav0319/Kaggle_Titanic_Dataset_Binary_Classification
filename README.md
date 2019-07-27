6b0d34c457eecf6354e0b574f57b8b26140efd7b
Mark Xavier <mark.j.xavier@gmail.com>
2019-07-27 11:38:27 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Kaggle_Titanic_Dataset_Binary_Classification
+
+I thought it'd be fun to brush up on some of the ML stuff I learned in my recent grad school course
+by playing around on Kaggle, and this competition looked like the perfect place to get warmed up.
+The "competition" is over [here](https://www.kaggle.com/c/titanic/overview).
---
+++ titanic.py
@@ -0,0 +1,24 @@
+"""
+titanic.py
+
+Not sure what I'll do here exactly yet, I need to run an exploratory analysis, feature engineer, and
+build models.  The last two I might do often more than once, so I'll need to figure out a good way
+to structure this project.
+
+Author: Mark Xavier
+"""
+
+def read_training_set():
+    """
+    """
+
+    pass
+
+def read_test_set():
+    """
+    """
+
+    pass
+
+if __name__ == '__main__':
+    pass
